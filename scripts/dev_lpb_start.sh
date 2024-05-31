ac8ec182ee599ca7440efeac8975aa2220211787
Henry Domagalski <henrycodev@gmail.com>
2024-06-01 00:46:54 +0200

    wip

--- scripts/dev_lpb_start.sh
+++ scripts/dev_lpb_start.sh
@@ -1,8 +1,41 @@
 #!/bin/bash
-sudo modprobe -r v4l2loopback
-sudo modprobe v4l2loopback video_nr=200,201
 
-nohup gst-launch-1.0 udpsrc port=5000 caps="application/x-rtp, media=video, clock-rate=90000, encoding-name=JPEG, payload=26" ! rtpjpegdepay ! jpegdec ! videoconvert ! v4l2sink device=/dev/video200 &
-nohup gst-launch-1.0 udpsrc port=5001 caps="application/x-rtp, media=video, clock-rate=90000, encoding-name=JPEG, payload=26" ! rtpjpegdepay ! jpegdec ! videoconvert ! v4l2sink device=/dev/video201 &
+# EXAMPLE INPUT OF A FILE:
+# [ID=PORT]
+# 200=5000
+# 201=5001
+#
+
+filename=$1
+
+if [[ ! -e "$filename" ]]; then
+    echo "Error: File $filename does not exist."
+    exit 1
+fi
+
+id_list=""
+declare -A id_port_map
+
+while IFS="=" read -r ID PORT || [[ -n "$ID" ]] ; do
+  # trim strings
+  ID=$(echo "$ID" | xargs)
+  PORT=$(echo "$PORT" | xargs)
+
+  if [ -z "$id_list" ]; then
+      id_list="$ID"
+  else
+      id_list="$id_list,$ID"
+  fi
+
+  id_port_map["$ID"]="$PORT"
+done < "$filename"
+
+sudo modprobe -r v4l2loopback
+sudo modprobe v4l2loopback video_nr="$id_list"
+
+for id in "${!id_port_map[@]}"; do
+  port="${id_port_map[$id]}"
+  nohup gst-launch-1.0 udpsrc port="$port" caps="application/x-rtp, media=video, clock-rate=90000, encoding-name=JPEG, payload=26" ! rtpjpegdepay ! jpegdec ! videoconvert ! v4l2sink device="/dev/video$id" &
+done
 
 echo "pipelines started"
--- scripts/dev_lpb_stop.sh
+++ scripts/dev_lpb_stop.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-kill $(pgrep -f "gst-launch-1.0 udpsrc port=")
-kill $(pgrep -f "dev_lpb_start.sh")
+sudo kill $(pgrep -f "gst-launch-1.0 udpsrc port=")
 sudo modprobe -r v4l2loopback
 echo "pipelines stopped"

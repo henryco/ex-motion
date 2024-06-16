ee3d61b115f04e769fe2bd940e770ce41d8579ad
henryco <henrycodev@gmail.com>
2024-06-17 01:48:19 +0200

    resource cleanup fix

--- sources/fbgtk/simple_image_window.cpp
+++ sources/fbgtk/simple_image_window.cpp
@@ -81,7 +81,7 @@ namespace xm {
     }
 
     void SimpleImageWindow::setFps(int _fps) {
-        const auto old = fps; // TODO: FIXME SEGFAULT ON EXIT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        const auto old = fps;
         fps = std::min(_fps, 1000);
         if (old != fps)
             fps_changed = true;

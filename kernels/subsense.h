390864729dc465b69bdd3044b73b570375c7854d
henryco <henrycodev@gmail.com>
2024-06-22 22:28:45 +0200

    wip

---
+++ kernels/subsense.h
@@ -0,0 +1,12 @@
+//
+// Created by henryco on 22/06/24.
+//
+
+#ifndef XMOTION_SUBSENSE_H
+#define XMOTION_SUBSENSE_H
+#include <cstddef>
+
+extern const char ocl_kernel_subsense_data[];
+extern const size_t ocl_kernel_subsense_data_size;
+
+#endif //XMOTION_SUBSENSE_H

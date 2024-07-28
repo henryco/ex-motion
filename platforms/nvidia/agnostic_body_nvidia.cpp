1dc11a1d625b0c695f8431c72fc56cb3768e16e4
henryco <henrycodev@gmail.com>
2024-07-28 18:06:22 +0200

    wip

--- platforms/nvidia/agnostic_body_nvidia.cpp
+++ platforms/nvidia/agnostic_body_nvidia.cpp
@@ -33,7 +33,55 @@ namespace platform::dnn {
             "landmark_heavy_q_f16.tflite",
             "landmark_full_q_f16.tflite",
             "landmark_lite_q_f16.tflite",
-    };
+        };
+    }
+
+    AgnosticBody::AgnosticBody(body::Model model) {
+        // TODO
+    }
+
+    AgnosticBody::~AgnosticBody() {
+        // TODO
+    }
+
+    body::Model AgnosticBody::get_model() const {
+        return body::Model::HEAVY_F32; // TODO
+    }
+
+    size_t AgnosticBody::get_in_w() const {
+        return 0; // TODO
+    }
+
+    size_t AgnosticBody::get_in_h() const {
+        return 0; // TODO
+    }
+
+    size_t AgnosticBody::get_n_lm3d() const {
+        return 0; // TODO
+    }
+
+    size_t AgnosticBody::get_n_segmentation_w() const {
+        return 0; // TODO
+    }
+
+    size_t AgnosticBody::get_n_segmentation_h() const {
+        return 0; // TOOD
+    }
+
+    void AgnosticBody::inference(size_t batch_size, const float *in_batch_ptr) {
+        // TODO
+    }
+
+    const float *const *AgnosticBody::get_segmentations() const {
+        return nullptr; // TODO
+    }
+
+    const float *const *AgnosticBody::get_landmarks_3d() const {
+        return nullptr; // TODO
+    }
+
+    const float *const *AgnosticBody::get_pose_flags() const {
+        return nullptr; // TODO
     }
 
 }
